0c38ce469f8d9600bb74914c2e7aaac6f75af8da
glecko <alex.muelas@gmail.com>
2021-05-15 18:11:04 +0200

    allow using cache values of higher depth while exploring at lower depths

--- profiling.py
+++ profiling.py
@@ -19,17 +19,17 @@ def profile_function(callback: Callable):
 
 
 game = Game()
-strategy_fn = build_tree_search_strategy(depth=3)
+strategy_fn = build_tree_search_strategy(depth=2)
 
 
 def function_to_profile():
-    strategy_fn(game.player_1, game.player_2, True)
-    #results = list()
-    #for _ in range(0, 10):
-    #    game = Game()
-    #    game.play_game(strategy_fn, strategy_fn)
-    #    results.append(game)
-    #analyze_games(results)
+    #strategy_fn(game.player_1, game.player_2, True)
+    results = list()
+    for _ in range(0, 10):
+        game = Game()
+        game.play_game(strategy_fn, strategy_fn)
+        results.append(game)
+    analyze_games(results)
 
 
 profile_function(function_to_profile)
--- strategies/tree_search/transposition.py
+++ strategies/tree_search/transposition.py
@@ -3,26 +3,35 @@ from strategies.tree_search.abstract_move import AbstractMove
 from typing import Optional, Dict, List, Tuple
 
 
-TRANSPOSITION_DICT: Dict[str, Tuple[List[AbstractMove], int]] = dict()
+TRANSPOSITION_DICT: Dict[str, Tuple[List[AbstractMove], int, int]] = dict()
 
 
-def get_board_depth_hash_key(player: Player, remaining_depth: int) -> str:
-    return f"{remaining_depth}#{player.id}#{player.board.to_string_notation()}"
+def get_board_depth_hash_key(player: Player) -> str:
+    return f"{player.id}#{player.board.to_string_notation()}"
 
 
 def get_best_move_from_transposition_dict(player: Player, remaining_depth: int, inverse_score: bool) -> Tuple[Optional[List[AbstractMove]], Optional[int]]:
-    board_hash_key = get_board_depth_hash_key(player, remaining_depth)
+    board_hash_key = get_board_depth_hash_key(player)
     if TRANSPOSITION_DICT.get(board_hash_key) is not None:
-        best_moves, best_score = TRANSPOSITION_DICT[board_hash_key]
-        if inverse_score:
-            return best_moves, -best_score
-        return best_moves, best_score
+        best_moves, best_score, explored_depth = TRANSPOSITION_DICT[board_hash_key]
+        if explored_depth >= remaining_depth:
+            if inverse_score:
+                return best_moves, -best_score
+            return best_moves, best_score
     return None, None
 
 
 def store_in_transposition_dict(best_moves: List[AbstractMove], best_score: int, player: Player, depth: int, inverse_score: bool):
-    board_hash_key = get_board_depth_hash_key(player, depth)
+    board_hash_key = get_board_depth_hash_key(player)
+
+    # We only overwrite the value in cache if our depth value is higher than the max explored depth
+    # already present in the cache
+    if TRANSPOSITION_DICT.get(board_hash_key) is not None:
+        _, _, explored_depth = TRANSPOSITION_DICT[board_hash_key]
+        if explored_depth >= depth:
+            return
+
     if inverse_score:
-        TRANSPOSITION_DICT[board_hash_key] = best_moves, -best_score
+        TRANSPOSITION_DICT[board_hash_key] = best_moves, -best_score, depth
     else:
-        TRANSPOSITION_DICT[board_hash_key] = best_moves, best_score
+        TRANSPOSITION_DICT[board_hash_key] = best_moves, best_score, depth
