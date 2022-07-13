14e6da3933e503c9bacc2de55b1575ffac030ae6
Alejandro Muelas <alex.muelas@gmail.com>
2022-07-13 18:10:40 +0200

    Fix build for MSVS 16 (#21)

--- cpp/src/game/include/game/PlacementMove.hpp
+++ cpp/src/game/include/game/PlacementMove.hpp
@@ -8,23 +8,22 @@
 namespace Alphalcazar::Game {
 	/*!
   	 * \brief Data structure with the minimnal information needed to uniquely represent a placement move.
-	 * 
+	 *
 	 * A placement move is defined as the action a player takes at the start of each turn,
 	 * where they take a piece they have in hand and place it on the perimeter of the board, facing
 	 * inwards.
 	 */
 	struct PlacementMove {
-		PlacementMove(const Coordinates& coordinates, PieceType pieceType)
-			: Coordinates{ coordinates }
-			, PieceType{ pieceType }
-		{}
+		PlacementMove();
+		PlacementMove(const Coordinates& coordinates, PieceType pieceType);
+		~PlacementMove();
 
 		Coordinates Coordinates;
 		PieceType PieceType;
 
-		bool operator==(const PlacementMove& other) const {
-			return PieceType == other.PieceType && Coordinates == other.Coordinates;
-		}
+		/// Returns whether the placement move isvalid
+		bool Valid() const;
+		bool operator==(const PlacementMove& other) const;
 	};
 
 }
