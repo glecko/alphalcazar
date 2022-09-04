de09ab4b0bdb0b01c3e1ef8d83ce777a0b0bc9ac
glecko <alex.muelas@gmail.com>
2022-09-04 11:16:11 +0200

    Inline PlacementMove constructors

--- cpp/src/game/src/game/PlacementMove.cpp
+++ cpp/src/game/src/game/PlacementMove.cpp
@@ -1,20 +1,10 @@
 #include "game/PlacementMove.hpp"
 
 namespace Alphalcazar::Game {
-	PlacementMove::PlacementMove()
-		: Coordinates{ Coordinates::Invalid() }
-		, PieceType{ 0 }
-	{}
-
-	PlacementMove::PlacementMove(const Game::Coordinates& coordinates, Game::PieceType pieceType)
-		: Coordinates{ coordinates }
-		, PieceType{ pieceType }
-	{}
-
-	PlacementMove::~PlacementMove() {}
-
 	bool PlacementMove::Valid() const {
-		return Coordinates.Valid() && PieceType != 0;
+		bool coordinatesValid = Coordinates.Valid();
+		bool pieceTypeValid = PieceType != 0;
+		return coordinatesValid && pieceTypeValid;
 	}
 
 	bool PlacementMove::operator==(const PlacementMove& other) const {
